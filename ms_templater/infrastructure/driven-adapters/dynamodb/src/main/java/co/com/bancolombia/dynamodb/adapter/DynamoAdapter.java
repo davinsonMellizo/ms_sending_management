93496c628efff0d59d27b2c5c6aacfa25b5f2331
vjojoa <vjojoa@bancolombia.com.co>
2022-01-13 15:34:32 -0500

    Configure Driven Adapter

--- ms_templater/infrastructure/driven-adapters/dynamodb/src/main/java/co/com/bancolombia/dynamodb/adapter/DynamoAdapter.java
+++ ms_templater/infrastructure/driven-adapters/dynamodb/src/main/java/co/com/bancolombia/dynamodb/adapter/DynamoAdapter.java
@@ -2,34 +2,15 @@ package co.com.bancolombia.dynamodb.adapter;
 
 import co.com.bancolombia.dynamo.AdapterOperations;
 import co.com.bancolombia.dynamodb.data.TemplateEmailData;
-import co.com.bancolombia.model.message.TemplateEmail;
-import co.com.bancolombia.model.message.gateways.PinpointGateway;
-
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 
 @Repository
-public class DynamoAdapter extends AdapterOperations<TemplateEmail, TemplateEmailData> implements PinpointGateway {
+public class DynamoAdapter extends AdapterOperations<TemplateEmailData> {
 
     public DynamoAdapter(final DynamoDbEnhancedAsyncClient client) {
         super(client);
     }
 
-    @Override
-    public Mono<TemplateEmail> findTemplateEmail(String templateName){
-        return findById(templateName);
-    }
-
-    public Mono<Void> saveTemplateEmail(TemplateEmail templateEmail){
-        return save(templateEmail);
-    }
-
-    public Mono<Void> updateTemplateEmail(TemplateEmail templateEmail){
-        return update(templateEmail);
-    }
-
-    public Mono<Void> deleteTemplateEmail(String templateName){
-        return delete(templateName);
-    }
 }
