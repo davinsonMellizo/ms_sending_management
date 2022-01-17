c19671664cecf97679d99c3f45ffb08f0a5dd75f
lumbauti <lumbauti@bancolombia.com.co>
2022-01-17 09:33:22 -0500

    update dynamo config3

--- ms_email_management/infrastructure/helpers/rabbit-configurator/src/main/java/co/com/bancolombia/rabbitmq/config/RabbitMQConfigHelper.java
+++ ms_email_management/infrastructure/helpers/rabbit-configurator/src/main/java/co/com/bancolombia/rabbitmq/config/RabbitMQConfigHelper.java
@@ -27,6 +27,7 @@ public class RabbitMQConfigHelper{
     private RabbitMQConnectionProperties rabbitProperties() {
         return secretsNameStandard.secretForRabbitMQ()
                 .flatMap(secretName -> secretsManager.getSecret(secretName, RabbitMQConnectionProperties.class))
+                .doOnNext(rabbit -> System.out.println("variables rabbit mq " + rabbit))
                 .block();
     }
 
@@ -35,6 +36,7 @@ public class RabbitMQConfigHelper{
     public ConnectionFactoryProvider getConnectionFactoryProvider(){
         RabbitMQConnectionProperties properties = rabbitProperties();
         final ConnectionFactory factory = new ConnectionFactory();
+        System.out.println("getConnectionFactoryProvider ++++ " + properties);
         PropertyMapper map = PropertyMapper.get();
 
         map.from(properties::getHostname).whenNonNull().to(factory::setHost);
