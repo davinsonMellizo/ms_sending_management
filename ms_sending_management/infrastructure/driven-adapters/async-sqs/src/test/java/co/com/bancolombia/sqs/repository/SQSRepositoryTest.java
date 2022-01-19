b7b06f84a4277439834c7aa33a489f9ba7672a1c
lumbauti <lumbauti@bancolombia.com.co>
2022-01-19 08:36:47 -0500

    update sqsconfig sending_management

--- ms_sending_management/infrastructure/driven-adapters/async-sqs/src/test/java/co/com/bancolombia/sqs/repository/SQSRepositoryTest.java
+++ ms_sending_management/infrastructure/driven-adapters/async-sqs/src/test/java/co/com/bancolombia/sqs/repository/SQSRepositoryTest.java
@@ -1,44 +1,39 @@
 package co.com.bancolombia.sqs.repository;
 
 import co.com.bancolombia.sqs.config.SQSProperties;
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.model.InvalidMessageContentsException;
-import com.amazonaws.services.sqs.model.SendMessageResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import reactor.test.StepVerifier;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+import java.util.concurrent.CompletableFuture;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class SQSRepositoryTest {
+
     @InjectMocks
     private SQSRepository sqsRepository;
     @Mock
-    private AmazonSQSAsync client;
+    private SqsAsyncClient client;
     @Mock
     private SQSProperties properties;
 
     @BeforeEach
     public void init(){
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
         when(properties.getUrl()).thenReturn("localhost");
     }
     @Test
     public void sendMessageTest(){
-        when(client.sendMessage(any())).thenReturn(new SendMessageResult());
+        when(client.sendMessage((SendMessageRequest) any()))
+                .thenReturn(new CompletableFuture<>());
         StepVerifier.create(sqsRepository.putQueue("message"))
                 .verifyComplete();
-    }
-
-    @Test
-    public void sendMessageErrorTest(){
-        when(client.sendMessage(any())).thenThrow(new InvalidMessageContentsException("error"));
-        StepVerifier.create(sqsRepository.putQueue("message"))
-                .expectError()
-                .verify();
     }
 }
