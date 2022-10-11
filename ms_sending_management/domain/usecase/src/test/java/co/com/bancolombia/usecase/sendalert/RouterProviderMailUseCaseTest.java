1d9b4a1208a552cd2938182a894391e169b5f663
dmellizo <dmellizo@bancolombia.com.co>
2022-10-11 08:37:42 -0500

    operación 1 sending

--- ms_sending_management/domain/usecase/src/test/java/co/com/bancolombia/usecase/sendalert/RouterProviderMailUseCaseTest.java
+++ ms_sending_management/domain/usecase/src/test/java/co/com/bancolombia/usecase/sendalert/RouterProviderMailUseCaseTest.java
@@ -52,6 +52,9 @@ class RouterProviderMailUseCaseTest {
         message.setPhone("32158967");
         message.setPhoneIndicator("57");
         message.setMail("bancolombia@com.co");
+        message.setRemitter("bancolombia@bancolombia.com.co");
+        message.setPriority(2);
+        message.setTemplate("compra");
         message.setAttachments(new ArrayList<>());
         ArrayList<Parameter> parameters = new ArrayList<>();
         Parameter parameter = Parameter.builder().Name("name").Value("bancolombia").build();
@@ -71,7 +74,6 @@ class RouterProviderMailUseCaseTest {
                 .build();
         when(commandGateway.sendCommandAlertEmail(any())).thenReturn(Mono.empty());
         when(logUseCase.sendLogMAIL(any(),any(), anyString(), any())).thenReturn(Mono.empty());
-        when(remitterGateway.findRemitterById(anyInt())).thenReturn(Mono.just(remitter));
         when(providerGateway.findProviderById(anyString())).thenReturn(Mono.just(provider));
         StepVerifier.create(routerProviderMailUseCase.routeAlertMail(message, alert))
                 .verifyComplete();
@@ -79,7 +81,6 @@ class RouterProviderMailUseCaseTest {
 
     @Test
     void routeAlertMailErrorTest(){
-        Remitter remitter = Remitter.builder().mail("bancolombia@com.co").build();
         Provider provider = Provider.builder().id("MAS").build();
         Alert alert = Alert.builder()
                 .push("SI")
@@ -88,7 +89,6 @@ class RouterProviderMailUseCaseTest {
                 .build();
         when(commandGateway.sendCommandAlertEmail(any())).thenReturn(Mono.error(new Throwable("error")));
         when(logUseCase.sendLogMAIL(any(),any(), anyString(), any())).thenReturn(Mono.empty());
-        when(remitterGateway.findRemitterById(anyInt())).thenReturn(Mono.just(remitter));
         when(providerGateway.findProviderById(anyString())).thenReturn(Mono.just(provider));
         StepVerifier.create(routerProviderMailUseCase.routeAlertMail(message, alert))
                 .expectError()
