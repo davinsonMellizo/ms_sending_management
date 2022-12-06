b848e26f1d92568558ac317b7538c2910cff1478
Ivan Ferney Rodriguez Rojas <ifrodrig@bancolombia.com.co>
2022-12-06 09:24:20 -0500

    mock

--- ms_sms_management/infrastructure/entry-points/async-event-handler/src/main/java/co/com/bancolombia/events/HandlerRegistryConfiguration.java
+++ ms_sms_management/infrastructure/entry-points/async-event-handler/src/main/java/co/com/bancolombia/events/HandlerRegistryConfiguration.java
@@ -2,9 +2,17 @@ package co.com.bancolombia.events;
 
 import co.com.bancolombia.events.handlers.CommandsHandler;
 import co.com.bancolombia.model.message.Alert;
+import org.reactivecommons.api.domain.Command;
 import org.reactivecommons.async.api.HandlerRegistry;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Retryable;
+
+import java.util.Map;
 
 import static co.com.bancolombia.events.commons.EventNameConfig.SEND_ALERT;
 import static org.reactivecommons.async.api.HandlerRegistry.register;
@@ -17,4 +25,8 @@ public class HandlerRegistryConfiguration {
         return register()
                 .handleCommand(SEND_ALERT, commands::handleSendAlert, Alert.class);
     }
-}
+
+
+    }
+
+
