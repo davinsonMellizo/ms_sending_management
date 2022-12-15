16d938fa543b17ef782d295bc985ad908de429d3
nasuarez <nasuarez@bancolombia.com.co>
2022-12-15 09:28:16 -0500

    Add Exception Campaign is inactive

--- ms_massive/domain/model/src/main/java/co/com/bancolombia/model/commons/enums/BusinessExceptionEnum.java
+++ ms_massive/domain/model/src/main/java/co/com/bancolombia/model/commons/enums/BusinessExceptionEnum.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum BusinessExceptionEnum {
     BUSINESS_CAMPAIGN_NOT_FOUND("BE001", "Campana no encontrada"),
-    BUSINESS_CAMPAIGN_WITHOUT_SCHEDULE_ON_DEMAND("BE002", "Campana sin horario bajo demanda");
+    BUSINESS_CAMPAIGN_WITHOUT_SCHEDULE_ON_DEMAND("BE002", "Campana sin horario bajo demanda"),
+    BUSINESS_CAMPAIGN_IS_INACTIVE("BE003", "La campana se encuentra desactivada");
 
     private final String code;
     private final String message;
--- ms_massive/domain/usecase/src/main/java/co/com/bancolombia/usecase/MassiveUseCase.java
+++ ms_massive/domain/usecase/src/main/java/co/com/bancolombia/usecase/MassiveUseCase.java
@@ -2,10 +2,13 @@ package co.com.bancolombia.usecase;
 
 import co.com.bancolombia.model.campaign.gateways.CampaignGateway;
 import co.com.bancolombia.model.campaign.gateways.CampaignGlueGateway;
+import co.com.bancolombia.model.commons.exception.BusinessException;
 import co.com.bancolombia.model.massive.Massive;
 import co.com.bancolombia.model.response.StatusResponse;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
+
+import static co.com.bancolombia.model.commons.enums.BusinessExceptionEnum.BUSINESS_CAMPAIGN_IS_INACTIVE;
 
 @RequiredArgsConstructor
 public class MassiveUseCase {
@@ -16,6 +19,8 @@ public class MassiveUseCase {
 
     public Mono<StatusResponse> sendCampaign(Massive massive) {
         return campaignRepository.findCampaignById(massive)
+                .filter(campaign -> campaign.getState().equals("1"))
+                .switchIfEmpty(Mono.error(new BusinessException(BUSINESS_CAMPAIGN_IS_INACTIVE)))
                 .flatMap(glueGateway::startTrigger)
                 .map(triggerName -> StatusResponse
                         .builder()
