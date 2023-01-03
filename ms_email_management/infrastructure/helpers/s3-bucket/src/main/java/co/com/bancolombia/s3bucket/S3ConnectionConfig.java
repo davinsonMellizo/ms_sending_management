fe2ed13fd328d2dde01322883c81b15ce8f3cac5
maquijan <maquijan@bancolombia.com.co>
2023-01-03 12:56:42 -0500

    update gradle

--- ms_email_management/infrastructure/helpers/s3-bucket/src/main/java/co/com/bancolombia/s3bucket/S3ConnectionConfig.java
+++ ms_email_management/infrastructure/helpers/s3-bucket/src/main/java/co/com/bancolombia/s3bucket/S3ConnectionConfig.java
@@ -11,6 +11,7 @@ import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvide
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.endpoints.Endpoint;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3AsyncClientBuilder;
@@ -18,6 +19,7 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.utils.ThreadFactoryBuilder;
 
 import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -49,7 +51,14 @@ public class S3ConnectionConfig {
                 .asyncConfiguration(config -> config.advancedOption(FUTURE_COMPLETION_EXECUTOR, threadPoolExecutor()))
                 .region(s3ConnectionProperties.getRegion());
         if (withEndpoint) {
-            s3AsyncClient.endpointOverride(URI.create(s3ConnectionProperties.getEndpoint()));
+            s3AsyncClient.endpointProvider(endpointParams ->
+                    CompletableFuture.completedFuture(Endpoint
+                            .builder()
+                            .url(URI.create(String.join("/", s3ConnectionProperties.getEndpoint(),
+                                    endpointParams.bucket())))
+                            .build()
+                    )
+            );
         }
         return s3AsyncClient.build();
     }
