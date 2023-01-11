50534e70ad3b735e1072633f4a9a5a9bce22f158
dmellizo <dmellizo@bancolombia.com.co>
2023-01-11 10:59:27 -0500

    refactor test

--- ms_sending_management/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/alertclient/data/AlertClientData.java
+++ ms_sending_management/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/alertclient/data/AlertClientData.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.data.annotation.Transient;
-import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
--- ms_sending_management/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/client/data/ClientData.java
+++ ms_sending_management/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/client/data/ClientData.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
