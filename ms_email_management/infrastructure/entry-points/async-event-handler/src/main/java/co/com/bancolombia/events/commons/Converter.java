dd3bdb5aef1bbc66185f78bf3286675ca762e8e3
Ivan Ferney Rodriguez Rojas <ifrodrig@bancolombia.com.co>
2023-01-12 16:52:38 -0500

    arreglo pruebas unitarias

--- ms_email_management/infrastructure/entry-points/async-event-handler/src/main/java/co/com/bancolombia/events/commons/Converter.java
+++ ms_email_management/infrastructure/entry-points/async-event-handler/src/main/java/co/com/bancolombia/events/commons/Converter.java
@@ -33,7 +33,7 @@ public abstract class  Converter {
         }
     }
 
-    protected Map<String ,Object> addHeader(Map<String ,Object> headers, String key, Object value){
+    private Map<String ,Object> addHeader(Map<String ,Object> headers, String key, Object value){
         if(headers==null ||headers.isEmpty()){
             headers= new HashMap<>();
         }
