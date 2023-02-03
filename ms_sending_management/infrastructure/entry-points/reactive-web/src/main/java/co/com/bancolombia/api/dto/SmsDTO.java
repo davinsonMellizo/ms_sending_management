9ae75bad7949033191e5508c2ccf61e3e3d330d7
Davinson Mellizo <dmellizo@bancolombia.com.co>
2023-02-03 15:16:31 -0500

    versions

--- ms_sending_management/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/SmsDTO.java
+++ ms_sending_management/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/SmsDTO.java
@@ -6,9 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
