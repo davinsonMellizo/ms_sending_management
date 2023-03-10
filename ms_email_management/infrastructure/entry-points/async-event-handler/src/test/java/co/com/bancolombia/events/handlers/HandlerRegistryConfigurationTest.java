a34c6096253df9aa906a7ce961f0d3e7e1e30f0d
Ivan Ferney Rodriguez Rojas <ifrodrig@bancolombia.com.co>
2023-03-10 10:57:03 -0500

    test (ms_sms_management): se agregan preubas unitarias

--- ms_email_management/infrastructure/entry-points/async-event-handler/src/test/java/co/com/bancolombia/events/handlers/HandlerRegistryConfigurationTest.java
+++ ms_email_management/infrastructure/entry-points/async-event-handler/src/test/java/co/com/bancolombia/events/handlers/HandlerRegistryConfigurationTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
-public class HandlerRegistryConfigurationTest {
+class HandlerRegistryConfigurationTest {
     @InjectMocks
     HandlerRegistryConfiguration configuration;
     @Mock
