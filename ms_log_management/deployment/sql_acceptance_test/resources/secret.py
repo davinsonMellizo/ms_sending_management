eef91939a71c43a990114c9364efa8d4a2a923ad
Davinson Mellizo <dmellizo@bancolombia.com.co>
2023-03-22 16:47:39 -0500

    test(ms_log_management): add role to boto3

--- ms_log_management/deployment/sql_acceptance_test/resources/secret.py
+++ ms_log_management/deployment/sql_acceptance_test/resources/secret.py
@@ -3,10 +3,22 @@ import boto3
 import json
 from botocore.exceptions import ClientError
 
-def get_secret(secretName):
+def get_secret(secretName, arnRole):
     secret_name = secretName
     region_name = "us-east-1"
 
+    sts_client = boto3.client('sts')
+    assumed_role_object=sts_client.assume_role(
+        RoleArn=arnRole,
+        RoleSessionName="AssumeRoleSession1",
+        DurationSeconds=1800
+    )
+    credentials=assumed_role_object['Credentials']
+    session = boto3.Session(
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken'],
+        )
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
